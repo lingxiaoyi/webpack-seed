59c0292fe9a542ecb4c1a9826b4930301ba5bb5b
zhijun <550843086@qq.com>
2017-09-20 10:39:15 +0800

    修改下目录

--- webpack-config/inherit/plugins.config.js
+++ webpack-config/inherit/plugins.config.js
@@ -5,6 +5,7 @@ let path = require('path')
 let dirlets = require('../base/dir-vars.config.js')
 let pageArr = require('../base/page-entries.config.js')
 let HashOutput = require('webpack-plugin-hash-output')
+const InlineChunkWebpackPlugin = require('html-webpack-inline-chunk-plugin')
 
 let configPlugins = [
     /* 全局shimming */
@@ -24,11 +25,14 @@ let configPlugins = [
     }),
     /* 抽取出webpack的runtime代码()，避免稍微修改一下入口文件就会改动commonChunk，导致原本有效的浏览器缓存失效 */
     new webpack.optimize.CommonsChunkPlugin({
-        name: 'webpack-runtime',
-        filename: 'static/commons/webpack-runtime.[hash].js'
+        name: 'manifest',
+        filename: '../webpack_manifest/manifest.[hash].js' //生成mainfest文件到build文件外  因为会将内容输出插入html script标签中,不需要额外引入或放到cdn里,,production环境可以用chuckhash,不过直接用hash省事,不用上边的复杂配置了
+    }),
+    new InlineChunkWebpackPlugin({
+        inlineChunks: ['manifest']
     }),
     /* 抽取出chunk的css */
-    new ExtractTextPlugin('[name]/styles.[contenthash].css'),
+    new ExtractTextPlugin('static/css/[name].[contenthash].css'),
     /* 配置好Dll */
     new webpack.DllReferencePlugin({
         context: dirlets.staticRootDir, // 指定一个路径作为上下文环境，需要与DllPlugin的context参数保持一致，建议统一设置为项目根目录
@@ -36,7 +40,7 @@ let configPlugins = [
         name: 'dll' // 当前Dll的所有内容都会存放在这个参数指定变量名的一个全局变量下，注意与DllPlugin的name参数保持一致
     }),
     new HashOutput({
-        manifestFiles: 'webpack-runtime' // 指定包含 manifest 在内的 chunk
+        manifestFiles: 'manifest' // 指定包含 manifest 在内的 chunk
     })
 ]
 
@@ -44,7 +48,7 @@ pageArr.forEach((page) => {
     const htmlPlugin = new HtmlWebpackPlugin({
         filename: `${page}.html`,
         template: path.resolve(dirlets.pagesDir, `./${page}/html`),
-        chunks: ['webpack-runtime', page, 'static/commons'],
+        chunks: ['manifest', page, 'static/commons'],
         hash: true, // 为静态资源生成hash值
         xhtml: true
     })
--- webpack.dev.soucemap.config.js
+++ webpack.dev.soucemap.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
     externals: require('./webpack-config/externals.config.js'),
 
-    devtool: 'inline-source-map',
+    devtool: 'cheap-module-eval-source-map', //inline-source-map
 
     devServer: require('./webpack-config/vendor/devServer.config.js')
 }
